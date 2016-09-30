c0a5731c55f3b8fd0b0c824252488c10c02e2e0e
Ivan <zjw@192-168-1-19.tpgi.com.au>
2016-09-30 20:52:27 +1000

    Add USER class
    Admin and Customer class extends the USER class

---
+++ src/Admin.java
@@ -0,0 +1,8 @@
+
+public class Admin extends User{
+
+	public Admin(int id, String name, String username, String password,
+			String dob) {
+		super(id, name, username, password, dob);
+	}
+}
---
+++ src/User.java
@@ -0,0 +1,128 @@
+
+public class User {
+
+	// customer id
+	private int id;
+	// customer real name
+	private String name;
+	// user name in system
+	private String username;
+	// customer password
+	private String password;
+	// security question
+	private String secrQues;
+	// security question answer
+	private String answer;
+	// date of birth
+	private String dob;
+	// customer address
+	private String address;
+	// card number
+	private String cardNumber;
+	// customer's phone number
+	private String phone;
+	
+
+	// User account constructor
+	public User(String name, String address, String cardNumber, String phone){
+		this.name = name;
+		this.address = address;
+		this.cardNumber = cardNumber;
+		this.phone = phone;
+	}
+	
+	// User constructor
+	public User(int id, String name, String username, String password,
+			String dob) {
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.dob = dob;
+		// current file does not contain following informatin
+		this.address = "";
+		this.phone = "";
+		this.cardNumber = "";
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getSecrQues() {
+		return secrQues;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setSecrQues(String secrQues) {
+		this.secrQues = secrQues;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+}
